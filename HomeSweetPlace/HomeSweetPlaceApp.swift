1aff5ee6eaa10778962db82080dd8210adf6b30c
Daniel Gutierrez <danielgudu22@gmail.com>
2023-07-23 11:00:36 -0500

    Initial Commit

---
+++ HomeSweetPlace/HomeSweetPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSweetPlaceApp.swift
+//  HomeSweetPlace
+//
+//  Created by Daniel Gutierrez on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeSweetPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
