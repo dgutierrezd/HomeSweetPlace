7088e4c525e20f31983dc5b0b77ed138e69b0e8e
Daniel Gutierrez <danielgudu22@gmail.com>
2023-08-18 16:17:04 -0500

    feat: change colors to white background

--- HomeSweetPlace/Auth/LoginView.swift
+++ HomeSweetPlace/Auth/LoginView.swift
@@ -18,75 +18,79 @@ struct LoginView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView(showsIndicators: false) {
-                Text("Iniciar Sesión").font(.system(size: 30).bold()).padding(.bottom, 30)
-                Spacer()
-                VStack {
-                    Text("Correo electrónico").frame(maxWidth: .infinity, alignment: .leading).padding(.bottom, 10)
-                    InputComponent(placeholder: "Correo electrónico", errorText: "Correo electrónico incorrecto", text: $username, email: true)
-                }.padding(.bottom, 25)
-                VStack {
-                    Text("Contraseña").frame(maxWidth: .infinity, alignment: .leading).padding(.bottom, 10)
-                    ZStack(alignment: .trailing) {
-                        Group {
-                            InputComponent(placeholder: "Contraseña", errorText: "Contraseña incorrecta",text: $password, password: !showPassword)
-                            Button(action: {
-                                showPassword.toggle()
-                            }) {
-                                Image(systemName: self.showPassword ? "eye.slash" : "eye")
-                                    .accentColor(.gray)
-                            }.padding(.trailing, 8)
+            ZStack {
+                Color.white.ignoresSafeArea()
+                ScrollView(showsIndicators: false) {
+                    Text("Iniciar Sesión").font(.system(size: 30).bold()).padding(.bottom, 30).foregroundColor(.black)
+                    Spacer()
+                    VStack {
+                        Text("Correo electrónico").frame(maxWidth: .infinity, alignment: .leading).padding(.bottom, 10).foregroundColor(.black)
+                        InputComponent(placeholder: "Correo electrónico", errorText: "Correo electrónico incorrecto", text: $username, email: true)
+                    }.padding(.bottom, 25)
+                    VStack {
+                        Text("Contraseña").frame(maxWidth: .infinity, alignment: .leading).padding(.bottom, 10)
+                            .foregroundColor(.black)
+                        ZStack(alignment: .trailing) {
+                            Group {
+                                InputComponent(placeholder: "Contraseña", errorText: "Contraseña incorrecta",text: $password, password: !showPassword)
+                                Button(action: {
+                                    showPassword.toggle()
+                                }) {
+                                    Image(systemName: self.showPassword ? "eye.slash" : "eye")
+                                        .accentColor(.gray)
+                                }.padding(.trailing, 8)
+                            }
                         }
                     }
-                }
-                HStack {
                     HStack {
-                        Image(systemName: remember ? "checkmark.square.fill" : "square")
-                            .foregroundColor(remember ? Color("secondary") : Color.secondary)
-                            .onTapGesture {
-                                        self.remember.toggle()
-                                    }
-                        Text("Recuérdame").font(.system(size: 14)).underline()
+                        HStack {
+                            Image(systemName: remember ? "checkmark.square.fill" : "square")
+                                .foregroundColor(remember ? Color("secondary") : Color.secondary)
+                                .onTapGesture {
+                                            self.remember.toggle()
+                                        }
+                            Text("Recuérdame").font(.system(size: 14)).underline().foregroundColor(.black)
+                        }
+                        Spacer()
+                        Text("Olvidaste la contraseña?").font(.system(size: 14)).underline().foregroundColor(.black)
+                    }.padding(.top, 30)
+                    Spacer()
+                    LargeButtonComponent(text: "Ingresar", disabled: username.isEmpty || password.isEmpty).padding(.top, 50)
+                    HStack {
+                        Divider().frame(width: 100, height: 1).background(.black)
+                        Spacer()
+                        Text("O ingresa con").bold().foregroundColor(.black)
+                        Spacer()
+                        Divider().frame(width: 100, height: 1).background(.black)
+                    }.padding(.top, 40)
+                    HStack {
+                        Button(action: {}, label: {
+                            Image("google").resizable().aspectRatio(contentMode: .fit).frame(width: 18)
+                        })
+                        .frame(width: 80, height: 40)
+                        .background(Color("secondary"))
+                        .cornerRadius(10)
+                        Spacer()
+                        Button(action: {}, label: {
+                            Image("apple").resizable().aspectRatio(contentMode: .fit).frame(width: 18)
+                        })
+                        .frame(width: 80, height: 40)
+                        .background(Color("secondary"))
+                        .cornerRadius(10)
+                        Spacer()
+                        Button(action: {}, label: {
+                            Image("facebook").resizable().aspectRatio(contentMode: .fit).frame(width: 11)
+                        })
+                        .frame(width: 80, height: 40)
+                        .background(Color("secondary"))
+                        .cornerRadius(10)
+                    }.padding(.bottom, 46).padding(.top, 60)
+                    HStack {
+                        Text("No tienes cuenta?").foregroundColor(.black)
+                        Text("¡Registrate!").foregroundColor(Color("primary"))
                     }
-                    Spacer()
-                    Text("Olvidaste la contraseña?").font(.system(size: 14)).underline()
-                }.padding(.top, 30)
-                Spacer()
-                LargeButtonComponent(text: "Ingresar", disabled: username.isEmpty || password.isEmpty).padding(.top, 50)
-                HStack {
-                    Divider().frame(width: 100, height: 1).background(.black)
-                    Spacer()
-                    Text("O ingresa con").bold()
-                    Spacer()
-                    Divider().frame(width: 100, height: 1).background(.black)
-                }.padding(.top, 40)
-                HStack {
-                    Button(action: {}, label: {
-                        Image("google").resizable().aspectRatio(contentMode: .fit).frame(width: 18)
-                    })
-                    .frame(width: 80, height: 40)
-                    .background(Color("secondary"))
-                    .cornerRadius(10)
-                    Spacer()
-                    Button(action: {}, label: {
-                        Image("apple").resizable().aspectRatio(contentMode: .fit).frame(width: 18)
-                    })
-                    .frame(width: 80, height: 40)
-                    .background(Color("secondary"))
-                    .cornerRadius(10)
-                    Spacer()
-                    Button(action: {}, label: {
-                        Image("facebook").resizable().aspectRatio(contentMode: .fit).frame(width: 11)
-                    })
-                    .frame(width: 80, height: 40)
-                    .background(Color("secondary"))
-                    .cornerRadius(10)
-                }.padding(.bottom, 46).padding(.top, 60)
-                HStack {
-                    Text("No tienes cuenta?")
-                    Text("¡Registrate!").foregroundColor(Color("primary"))
-                }
-            }.padding().ignoresSafeArea(.keyboard).scrollDismissesKeyboard(.interactively)
+                }.padding().ignoresSafeArea(.keyboard).scrollDismissesKeyboard(.interactively)
+            }
         }
         .navigationBarItems(leading: Button(action : {
             self.mode.wrappedValue.dismiss()
--- HomeSweetPlace/ContentView.swift
+++ HomeSweetPlace/ContentView.swift
@@ -10,25 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                Spacer()
-                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 70).padding()
-                Text("Home Sweet Place").font(.custom(
-                    "Apple SD Gothic Neo", size: 18).weight(.ultraLight))
-                Spacer()
-                NavigationLink(destination: LoginView()) {
-                    Text("Iniciar Sesión")
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding().background(Color("primary"))
-                        .cornerRadius(19)
-                        .padding(.bottom, 30)
-                }
-                Button(action: {}) {
+            ZStack {
+                Color.white.ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 70).padding()
+                    Text("Home Sweet Place").font(.custom(
+                        "Apple SD Gothic Neo", size: 18).weight(.ultraLight)).foregroundColor(.black)
+                    Spacer()
+                    NavigationLink(destination: LoginView()) {
+                        Text("Iniciar Sesión")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding().background(Color("primary"))
+                            .cornerRadius(19)
+                            .padding(.bottom, 30)
+                    }
                     Text("Registrarme").fontWeight(.bold).foregroundColor(Color("secondary"))
                 }
+                .padding()
             }
-            .padding()
         }
     }
 }
